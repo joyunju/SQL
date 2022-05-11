ea5a1e0a8c017e453a2d21fe722f32fb64f2c4b1
joyunju <whdbswn1113@naver.com>
2022-05-11 15:53:17 +0900

    ELECT문 기본

---
+++ Ex01.sql
@@ -0,0 +1,15 @@
+--일반 행 주석 : ctrl + /
+/*범위 주석 : alt + shift + c*/
+-- 자동 정렬 : ctrl + f7
+-- SELECT문 기본 
+
+SELECT
+    *
+FROM
+    employees;
+
+SELECT
+    *
+FROM
+    departments;
+--범위만 선택해서 쿼리 실행 가능! / 원하는 범위 커서 두고 ctrl + enter 가능 
